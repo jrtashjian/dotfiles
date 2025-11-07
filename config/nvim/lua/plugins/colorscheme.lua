56a56291c78dc02cffcf18ff4ab99a789f20b7a1
JR Tashjian <hello@jrtashjian.com>
2025-11-06 23:52:31 -0500

    Add nvim config

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+	"Mofiqul/dracula.nvim",
+	config = function()
+		local config = require("dracula")
+		config.setup({
+			transparent_bg = true,
+		})
+
+		vim.cmd([[colorscheme dracula]])
+	end,
+}
