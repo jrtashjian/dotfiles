d8c844a9bd359fb9a6fc945dffea4c90b88c2115
JR Tashjian <hello@jrtashjian.com>
2025-11-08 00:52:05 -0500

    Some more keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -56,4 +56,19 @@ vim.o.ruler = false           -- Hide ruler
 vim.o.ignorecase = true       -- Case-insensitive search
 vim.o.smartcase = true        -- Case-sensitive if uppercase used
 
+-- Keymapping
+local map = vim.keymap.set
+
+map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "clear highlights" })
+
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "save file" })
+
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+
 require("lazy").setup("plugins")
