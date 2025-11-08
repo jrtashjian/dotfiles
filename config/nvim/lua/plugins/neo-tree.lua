f6c0fa5d5c1dae885bae55b79797e55fcce1d9be
JR Tashjian <hello@jrtashjian.com>
2025-11-08 00:13:44 -0500

    improve neo-tree config

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -5,8 +5,27 @@ return {
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 	},
+    lazy = false,
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left <CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float <CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>", { desc = "toggle file explorer" })
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem focus<CR>", { desc = "focus file explorer" })
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { desc = "show open editors" })
+
+        require("neo-tree").setup({
+            window = {
+                position = "right",
+            },
+            filesystem = {
+                filtered_items = {
+                    hide_gitignored = false,
+                    always_show = {
+                        ".gitignore",
+                    },
+                    always_show_by_pattern = {
+                        ".env*"
+                    },
+                },
+            },
+        })
 	end,
 }
