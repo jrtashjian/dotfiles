7ef174f2056a70cea5ec21395287264169393923
JR Tashjian <hello@jrtashjian.com>
2025-11-08 10:52:35 -0500

    More keymaps

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -1,14 +1,40 @@
 -- Keymapping
 local map = vim.keymap.set
 
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
 
-map("n", "<Esc>", "<cmd>noh<CR>", { desc = "clear highlights" })
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
-map("n", "<C-s>", "<cmd>w<CR>", { desc = "save file" })
+-- 
+
+-- Move Lines
+map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+-- Buffers
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+
+map("n", "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+map("x", "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+map("s", "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+map("i", "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+map("n", "<Esc>", "<cmd>noh<cr>", { desc = "Clear Highlights" })
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 
 map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
 map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
