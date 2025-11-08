36b5f89e86fa01869b308a682e1c7e526dcfc951
JR Tashjian <hello@jrtashjian.com>
2025-11-08 01:00:37 -0500

    Improve telescope config

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -4,9 +4,27 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local builtin = require("telescope.builtin")
+
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+        require("telescope").setup({
+            defaults = {
+                prompt_prefix = "   ",
+                selection_caret = " ",
+                entry_prefix = " ",
+                sorting_strategy = "ascending",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        preview_width = 0.55,
+                    },
+                    width = 0.87,
+                    height = 0.80,
+                },
+            },
+        })
 	end,
 }
