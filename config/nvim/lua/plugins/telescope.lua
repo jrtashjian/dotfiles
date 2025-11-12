c03667d8771dcee050468576413a3c0f38ea0fd5
JR Tashjian <hello@jrtashjian.com>
2025-11-11 22:40:29 -0500

    reformat

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,32 +1,33 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require("telescope.builtin")
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require("telescope.builtin")
 
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
-        require("telescope").setup({
-            defaults = {
-                prompt_prefix = "   ",
-                selection_caret = " ",
-                entry_prefix = " ",
-                sorting_strategy = "ascending",
-                layout_config = {
-                    horizontal = {
-                        prompt_position = "top",
-                        preview_width = 0.55,
+            require("telescope").setup({
+                defaults = {
+                    prompt_prefix = "   ",
+                    selection_caret = " ",
+                    entry_prefix = " ",
+                    sorting_strategy = "ascending",
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "top",
+                            preview_width = 0.55,
+                        },
+                        width = 0.87,
+                        height = 0.80,
                     },
-                    width = 0.87,
-                    height = 0.80,
                 },
-            },
-        })
-	end,
+            })
+        end,
     },
     {
         "nvim-telescope/telescope-ui-select.nvim",
