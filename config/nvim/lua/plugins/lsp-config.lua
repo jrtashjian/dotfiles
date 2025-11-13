c09ffd6166ac550924ede32c9c0ac10b7925e34f
JR Tashjian <hello@jrtashjian.com>
2025-11-12 22:27:25 -0500

    setup none-ls for linting and formatting

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls", "stylua" }
             })
         end,
     },
@@ -17,6 +17,36 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             vim.diagnostic.config({ virtual_text = true })
+        end
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+            require('null-ls').setup({
+                -- format document on save using only null-ns.
+                on_attach = function(client, bufnr)
+                    if client.supports_method('textDocument/formatting') then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                vim.lsp.buf.format({
+                                    async = false,
+                                    bufnr = bufnr,
+                                    filter = function(formatter)
+                                        return formatter.name == "null-ls"
+                                    end
+                                })
+                            end,
+                        })
+                    end
+                end
+            })
+            -- manually format buffer content.
+            vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
         end
     },
 }
