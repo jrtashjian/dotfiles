575c37679608758d9544fd76a6b0c8767f7cde1c
JR Tashjian <hello@jrtashjian.com>
2025-11-13 15:48:08 -0500

    lualine tweaks

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -8,8 +8,8 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diagnostics" },
-				lualine_c = { "filename" },
-				lualine_x = { "lsp_status", "filetype" },
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = {},
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
