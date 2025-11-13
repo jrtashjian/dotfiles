6ddbf99f8059211ae505569cde95f70de0fcd812
JR Tashjian <hello@jrtashjian.com>
2025-11-12 23:00:31 -0500

    configure lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,18 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        require('lualine').setup({
-            options = {
-                theme = "dracula"
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "lsp_status", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
