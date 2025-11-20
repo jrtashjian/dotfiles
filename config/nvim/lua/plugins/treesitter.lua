87beb4531f52ef9d16e1ce769b02fc80259ec2f0
JR Tashjian <hello@jrtashjian.com>
2025-11-20 12:41:52 -0500

    fix treesitter configuration

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,33 +3,34 @@ return {
 	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
-	opts = {
-		auto_install = true,
-		hightlight = { enable = true },
-		indent = { enable = true },
-		ensure_installed = {
-			"lua",
-			"vim",
-			"vimdoc",
-			"python",
-			"javascript",
-			"typescript",
-			"sql",
-			"bash",
-			"css",
-			"diff",
-			"dockerfile",
-			"html",
-			"json",
-			"markdown",
-			"terraform",
-			"yaml",
-			"php",
-		},
-	},
-	init = function()
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"python",
+				"javascript",
+				"typescript",
+				"sql",
+				"bash",
+				"css",
+				"diff",
+				"dockerfile",
+				"html",
+				"json",
+				"markdown",
+				"terraform",
+				"yaml",
+				"php",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+
 		vim.wo.foldmethod = "expr"
-		vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 		vim.opt.foldlevel = 99
 	end,
 }
