64fefe6e1deb3c02eb172e6c45a00b0a887eb929
JR Tashjian <hello@jrtashjian.com>
2026-05-25 01:03:03 -0400

    Add key mapping for notification history in Snacks.nvim

--- config/nvim/lua/plugins/uiux.lua
+++ config/nvim/lua/plugins/uiux.lua
@@ -59,6 +59,7 @@ return {
             { "<leader><space>", function() Snacks.picker.smart() end,                desc = "Smart Find Files" },
             { "<leader>,",       function() Snacks.picker.buffers() end,              desc = "Buffers" },
             { "<leader>:",       function() Snacks.picker.command_history() end,      desc = "Command History" },
+            { "<leader>n",       function() Snacks.picker.notifications() end,        desc = "Notification History" },
             { "<leader>e",       function() Snacks.explorer() end,                    desc = "File Explorer" },
 
             -- find
