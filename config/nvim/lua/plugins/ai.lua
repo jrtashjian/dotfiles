f5f5087a320608583a24a88d649d7879f9a5aa0a
JR Tashjian <hello@jrtashjian.com>
2026-05-25 01:01:00 -0400

    Add opencode event handlers

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -39,6 +39,38 @@ return {
 
 			vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end, { desc = "Add range to opencode", expr = true })
 			vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+
+            -- Handle `opencode` events
+            vim.api.nvim_create_autocmd("User", {
+              pattern = "OpencodeEvent:*", -- Optionally filter event types
+              callback = function(args)
+                local event = args.data.event
+
+                -- if event.type == "permission.asked" then
+                --     vim.notify(vim.inspect(event), vim.log.levels.INFO, { timeout = false, title = "Opencode" })
+                --     vim.notify("`opencode` is asking for permission to execute code", vim.log.levels.WARN)
+                -- end
+
+                -- if event.type == "session.error" then
+                --     vim.notify(vim.inspect(event), vim.log.levels.INFO, { timeout = false, title = "Opencode" })
+                --     vim.notify("`opencode` session error: " .. event.data.error, vim.log.levels.ERROR)
+                -- end
+
+                if event.type == "question.asked" then
+                    vim.notify(vim.inspect(event), vim.log.levels.INFO, { timeout = false, title = "Opencode" })
+                    vim.notify("`opencode` is asking a question: ", vim.log.levels.INFO)
+                end
+
+                -- Notify when `opencode` finishes responding to an `ask()` or `select()` call
+                if event.type == "session.idle" then
+                    vim.notify("Finished responding", vim.log.levels.INFO, { title = "Opencode" })
+                end
+
+                if event.type == "server.connected" then
+                    vim.notify("Connected to server", vim.log.levels.INFO, { title = "OpenCode" })
+                end
+              end,
+            })
 		end,
 	},
 }
